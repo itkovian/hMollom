1f3ce6d193ec05c6e0d6b5a013a8c24821ed98c6
Andy Georges <itkovian@gmail.com>
2012-01-16 15:54:54 +0100

    Added the runMollom function to unwrap results

--- src/Network/Mollom/MollomMonad.hs
+++ src/Network/Mollom/MollomMonad.hs
@@ -3,6 +3,7 @@
 -- We wrap the configuration in a Reader
 module Network.Mollom.MollomMonad 
   ( Mollom
+  , MollomState
   , mollomService
   ) where
 
@@ -15,12 +16,13 @@ import Network.Mollom.Internals
 import Network.Mollom.Types
 
 type ContentID = String
+type MollomState = Maybe ContentID
 
 -- | The MollomMonad type is a monad stack that can retain the content ID in its
 --   state (Content, Captcha and Feedback APIs). We also need to have a configuration
 --   that's towed along with the public and private keys.
 newtype Mollom a = M { runM :: ErrorT MollomError 
-                                      (StateT (Maybe ContentID) 
+                                      (StateT MollomState
                                               (ReaderT MollomConfiguration IO)) a 
                      } deriving (Monad, MonadIO, MonadReader MollomConfiguration, MonadState (Maybe ContentID))
 
@@ -37,8 +39,12 @@ mollomService :: String             -- ^ Public key
 mollomService pubKey privKey method path params expected = 
     wrapMollom $ service pubKey privKey method path params expected
 
-runMollom :: Mollom a -> MollomConfiguration -> Maybe Int -> IO (Either MollomError (Maybe ContentID, a))
-runMollom m mc id = undefined
+runMollom :: Mollom a -> MollomConfiguration -> MollomState -> IO (Either MollomError (Maybe ContentID, a))
+runMollom m config state = do
+    v <- runReaderT (runStateT (runErrorT $ runM m) state) config
+    return $ case v of 
+                 (Left err, _) -> Left err
+                 (Right r, cid) -> Right (cid, r)
 
 --mkMollom :: MollomConfiguration -> Maybe Int -> Mollom a
 --mkMollom 
