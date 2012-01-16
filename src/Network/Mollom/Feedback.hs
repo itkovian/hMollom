ae47e8cc81f187c1661136e962255dd616a41ab5
Andy Georges <itkovian@gmail.com>
2012-01-16 10:33:01 +0100

    Added the Feedback API calls.
    The arguments for the feedback API require at least one of the
    content ID or the captcha ID. For now, we return an undefined, but
    we will change this to avoiding making the call to Mollom, instead
    immediately returning the appropriate error.

--- src/Network/Mollom/Feedback.hs
+++ src/Network/Mollom/Feedback.hs
@@ -6,7 +6,9 @@
  -}
 
 module Network.Mollom.Feedback
-  () where
+  ( Reason(..)
+  ) where
+
 import Control.Monad.Error
 import Control.Monad.Reader
 import Network.HTTP.Base (RequestMethod(..))
@@ -14,3 +16,40 @@ import Network.HTTP.Base (RequestMethod(..))
 import Network.Mollom.Internals
 import Network.Mollom.MollomMonad
 import Network.Mollom.Types
+
+
+-- | Data type representing the reasons the user can provide to
+--   the Mollom service for blacklisting a string or a value.
+data Reason = Approve
+            | Spam 
+            | Profanity 
+            | Quality 
+            | Unwanted
+            | Delete
+            deriving (Eq)
+
+instance Show Reason where
+  show Approve = "approve"
+  show Spam = "spam"
+  show Profanity = "profanity"
+  show Quality = "quality"
+  show Unwanted = "unwanted"
+  show Delete = "delete"
+
+
+sendFeedback :: Maybe String  -- ^Existing content ID
+             -> Maybe String  -- ^Existing captcha ID
+             -> Reason        -- ^Reason of the feedback
+             -> Mollom MollomResponse
+sendFeedback contentId captchaId reason = do
+    case contentId `mplus` captchaId of
+      Nothing -> undefined -- this should be an error
+      Just _  -> do config <- ask
+                    let pubKey = mcPublicKey config
+                        privKey = mcPrivateKey config
+                        path = "feedback"
+                        kvs = catSecondMaybes [ ("contentId", contentId)
+                                              , ("captchaId", captchaId)
+                                              ]
+                    Mollom $ ErrorT . liftIO . runErrorT $ service pubKey privKey POST path kvs []
+
