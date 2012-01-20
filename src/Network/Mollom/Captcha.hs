c55d61dbf5be27de0d9f9f0cdc99fb358e372353
Andy Georges <itkovian@gmail.com>
2012-01-20 02:12:34 +0100

    Added CaptchaResponse type and the FromJSON instance for it

--- src/Network/Mollom/Captcha.hs
+++ src/Network/Mollom/Captcha.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-
  - (C) 2012, Andy Georges
  -
@@ -7,10 +9,12 @@
 
 module Network.Mollom.Captcha
   ( Type(..)
+  , CaptchaResponse(..)
   , createCaptcha
   , verifyCaptcha
   ) where
 
+import Control.Applicative
 import Control.Monad.Error
 import Control.Monad.Reader
 import qualified Data.Aeson as A
@@ -31,11 +35,49 @@ instance Show Type where
   show Image = "image"
   show Audio = "audio"
 
-createCaptcha :: A.FromJSON a
-              => Type         -- ^ The type of captcha that should be created.
+instance A.FromJSON Type where
+    parseJSON (A.String s) = return $ case s of
+                                "image" -> Image
+                                "audio" -> Audio
+    parseJSON _ = mzero
+
+
+-- | Data type representing the response to a captcha API call
+data CaptchaResponse = 
+     CaptchaResponse { captchaId           :: String
+                     , captchaUrl          :: String
+                     , captchaSolved       :: Bool
+                     , captchaReason       :: String
+                     , captchaAuthorName   :: String
+                     , captchaAuthorUrl    :: String
+                     , captchaAuthorMail   :: String
+                     , captchaAuthorIP     :: String
+                     , captchaAuthorId     :: String
+                     , captchaAuthorOpenId :: String
+                     }
+
+
+instance A.FromJSON CaptchaResponse where
+    parseJSON j = do
+        o <- A.parseJSON j
+        s <- o A..: "captcha"
+        CaptchaResponse <$>
+          s A..: "captchaId" <*>
+          s A..: "url" <*>
+          s A..: "solved" <*>
+          s A..: "reason" <*>
+          s A..: "authorName" <*>
+          s A..: "authorUrl" <*>
+          s A..: "authorMail" <*>
+          s A..: "authorIp" <*>
+          s A..: "authorId" <*>
+          s A..: "authorOpenId"
+
+
+createCaptcha :: Type         -- ^ The type of captcha that should be created.
               -> Maybe Bool   -- ^ Use SSL if True (only for paid subscriptions).
               -> Maybe String -- ^ Optional content ID to refer to.
-              -> Mollom (MollomResponse a)
+              -> Mollom (MollomResponse CaptchaResponse)
 createCaptcha t ssl captchId = do
     config <- ask
     let pubKey = mcPublicKey config
@@ -50,8 +92,7 @@ createCaptcha t ssl captchId = do
 
 
 
-verifyCaptcha :: A.FromJSON a
-              => String         -- ^The captcha ID to be verified.
+verifyCaptcha :: String         -- ^The captcha ID to be verified.
               -> Maybe String   -- ^The name of the content author filling out the captcha.
               -> Maybe String   -- ^The website URL of the content author .
               -> Maybe String   -- ^The email address of the content author.
@@ -60,7 +101,7 @@ verifyCaptcha :: A.FromJSON a
               -> Maybe String   -- ^Content author's unique local site user ID.
               -> Maybe Int      -- ^The time that must have passed before the same author can post again. Defaults to 15.
               -> Maybe String   -- ^Client-side honeypot value, if any.
-              -> Mollom (MollomResponse a)
+              -> Mollom (MollomResponse CaptchaResponse)
 verifyCaptcha captchId authorName authorURL authorEmail authorOpenIds authorIP authorSiteID rateLimit honeypot = do
     config <- ask
     let pubKey = mcPublicKey config
