0928186208e473f68fc8408f88f9112940b5d0bd
Andy Georges <itkovian@gmail.com>
2012-01-20 01:43:17 +0100

    Added BlacklistResponse type and the FromJSON instance for it

--- src/Network/Mollom/Blacklist.hs
+++ src/Network/Mollom/Blacklist.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 {- 
  - (C) 2012, Andy Georges
  -
@@ -8,6 +10,7 @@ module Network.Mollom.Blacklist
   ( Reason(..)
   , Context(..)
   , Match(..)
+  , BlacklistResponse(..)
   , createBlacklist
   , updateBlacklist
   , deleteBlacklist
@@ -15,6 +18,7 @@ module Network.Mollom.Blacklist
   , readBlacklistEntry
   ) where
 
+import Control.Applicative
 import Control.Monad.Error
 import Control.Monad.Reader
 import qualified Data.Aeson as A
@@ -40,6 +44,13 @@ instance Show Reason where
   show Quality = "quality"
   show Unwanted = "unwanted"
 
+instance A.FromJSON Reason where
+    parseJSON (A.String s) = return $ case s of
+                               "spam"      -> Spam
+                               "profanity" -> Profanity
+                               "quality"   -> Quality
+                               "unwanted"  -> Unwanted
+    parseJSON _ = mzero
 
 -- | Data type representing the context in which the Mollom
 --   service is allowed to look for a blacklisted term (to be 
@@ -62,6 +73,18 @@ instance Show Context where
   show Links = "links"
   show PostTitle = "postTitle"
 
+instance A.FromJSON Context where
+    parseJSON (A.String s) = return $ case s of
+                                "allFields" -> AllFields
+                                "authorName" -> AuthorName
+                                "authorMail" -> AuthorMail
+                                "authorIp"   -> AuthorIp
+                                "authorId"   -> AuthorId
+                                "links"      -> Links
+                                "postTitle"  -> PostTitle
+    parseJSON _ = mzero
+
+
 -- | Data type indicating how well a specific match should be.
 data Match = Exact
            | Contains
@@ -71,17 +94,59 @@ instance Show Match where
   show Exact = "exact"
   show Contains = "contains"
 
+instance A.FromJSON Match where
+    parseJSON (A.String s) = return $ case s of
+                                "exact" -> Exact
+                                "contains" -> Contains
+    parseJSON _ = mzero
+
+
+-- | Data type representing the response in the blacklist API.
+data BlacklistResponse = 
+     BlacklistResponse { blacklistId         :: String
+                       , blacklistCreated    :: String -- FIXME should be datetime
+                       , blacklistStatus     :: Bool
+                       , blacklistLastMatch  :: String -- FIXME should be datetime
+                       , blacklistMatchCount :: Int
+                       , blacklistValue      :: String
+                       , blacklistReason     :: Reason
+                       , blacklistContext    :: Context
+                       , blacklistMatch      :: Match
+                       , blacklistNote       :: String
+                       }
+
+instance A.FromJSON BlacklistResponse where
+    parseJSON j = do
+        o <- A.parseJSON j
+        e <- o A..: "entry"
+        BlacklistResponse <$>
+          e A..: "id"         <*>
+          e A..: "created"    <*>
+          e A..: "status"     <*>
+          e A..: "lastMatch"  <*>
+          e A..: "matchCount" <*>
+          e A..: "value"      <*>
+          e A..: "reason"     <*>
+          e A..: "context"    <*>
+          e A..: "match"      <*>
+          e A..: "note"
+
+
+instance A.FromJSON [BlacklistResponse] where
+    parseJSON j = do
+      o <- A.parseJSON j
+      ls <- o A..: "list"
+      mapM A.parseJSON ls
 
 
 -- | Create a blacklist entry for the given site.
-createBlacklist :: A.FromJSON a
-                => String        -- ^ The value or string to blacklist
+createBlacklist :: String        -- ^ The value or string to blacklist
                 -> Maybe Reason  -- ^ The reason for this blacklisting
                 -> Maybe Context -- ^ Where may the entry match
                 -> Maybe Match   -- ^ How precise should the match be
                 -> Maybe Bool    -- ^ Is the entry live or not
                 -> Maybe String  -- ^ Note
-                -> Mollom (MollomResponse a)
+                -> Mollom (MollomResponse BlacklistResponse)
 createBlacklist s reason context match status note = do
     config <- ask
     let pubKey = mcPublicKey config
@@ -100,15 +165,14 @@ createBlacklist s reason context match status note = do
 
 -- | Update an existing blacklist entry. All arguments that are provided as Nothing
 --   default to keeping existing values.
-updateBlacklist :: A.FromJSON a
-                => String        -- ^ ID of the blacklisted entry to update
+updateBlacklist :: String        -- ^ ID of the blacklisted entry to update
                 -> Maybe String  -- ^ The blacklisted string or value.
                 -> Maybe Reason  -- ^ The reason for this blacklisting
                 -> Maybe Context -- ^ Where may the entry match
                 -> Maybe Match   -- ^ How precise should the match be
                 -> Maybe Bool    -- ^ Is the entry live or not
                 -> Maybe String  -- ^ Note
-                -> Mollom (MollomResponse a)
+                -> Mollom (MollomResponse ())
 updateBlacklist entryId s reason context match status note = do
     config <- ask
     let pubKey = mcPublicKey config
@@ -125,9 +189,8 @@ updateBlacklist entryId s reason context match status note = do
     mollomService pubKey privKey POST path kvs [] errors
 
 -- | Delete a blacklisted entry.
-deleteBlacklist :: A.FromJSON a
-                => String    -- ^ ID of the blacklisted entry to delete
-                -> Mollom (MollomResponse a)
+deleteBlacklist :: String    -- ^ ID of the blacklisted entry to delete
+                -> Mollom (MollomResponse ())
 deleteBlacklist entryId = do
     config <- ask
     let pubKey = mcPublicKey config
@@ -140,10 +203,9 @@ deleteBlacklist entryId = do
 -- | List the entries in the blacklist for a given set of credentials, 
 --   identified by the site public key.
 --   FIXME: the arguments determination is fugly.
-listBlacklist :: A.FromJSON a
-                => Maybe Int  -- ^ The offset from which to start listing entries. Defaults to 0 when Nothing is given as the argument.
+listBlacklist :: Maybe Int  -- ^ The offset from which to start listing entries. Defaults to 0 when Nothing is given as the argument.
               -> Maybe Int  -- ^ The number of entries that should be returned. Defaults to all.
-              -> Mollom (MollomResponse a)
+              -> Mollom (MollomResponse [BlacklistResponse])
 listBlacklist offset count = do
     config <- ask
     let pubKey = mcPublicKey config
@@ -159,9 +221,8 @@ listBlacklist offset count = do
 
 
 -- | Read the information that is stored for a given blacklist entry.
-readBlacklistEntry :: A.FromJSON a
-                => String  -- ^ ID of the blacklisted entry to read
-                   -> Mollom (MollomResponse a)
+readBlacklistEntry :: String  -- ^ ID of the blacklisted entry to read
+                   -> Mollom (MollomResponse BlacklistResponse)
 readBlacklistEntry entryId = do
     config <- ask
     let pubKey = mcPublicKey config
