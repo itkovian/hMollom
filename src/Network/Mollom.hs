6504cc5f98adf67b7d06a120eaca1132f10eaf88
Andy Georges <itkovian@gmail.com>
2012-01-27 17:45:53 +0100

    Exports fixored

--- src/Network/Mollom.hs
+++ src/Network/Mollom.hs
@@ -3,29 +3,10 @@
 -- | Interface to the Mollom web service (http://mollom.com),
 --   based on the beta REST API, see http://mollom.com/api/rest.
 module Network.Mollom
-  ( Mollom
-  , runMollom
-  , MollomConfiguration
-  -- site API
-  , readSite
-  , deleteSite
-  , listSites
-  -- captcha API
-  , createCaptcha
-  , verifyCaptcha
-  -- content API
-  , checkContent
+  ( M.runMollom
   ) where
 
-import Network.Mollom.Blacklist
-import Network.Mollom.Content
-import Network.Mollom.Captcha
-import Network.Mollom.Feedback
-import Network.Mollom.Site
-import Network.Mollom.MollomMonad
-import Network.Mollom.Whitelist
-import Network.Mollom.Types
-
+import qualified Network.Mollom.MollomMonad as M
 
 
 
